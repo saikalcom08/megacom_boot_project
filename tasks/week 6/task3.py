131fbfb33936d4b3b04baf5d49f6f444ba80ae41
saikalcom08 <saikalcom08@gmail.com>
2021-10-26 12:10:26 +0100

    update task3

--- tasks/week 6/task3.py
+++ tasks/week 6/task3.py
@@ -1,35 +1,34 @@
 class Product:
     def __init__(self, name, price, weight):
-        self._name = name
-        self._price = price
-        self._weight = weight
+        self.__name = name
+        self.__price = price
+        self.__weight = weight
 
     def get_price(self):
-        return self._price
+        return self.__price
 
     def get_weight(self):
-        return self._weight
+        return self.__weight
 
     def show_goods(self):
-        print(f"Good name: {self._name}\n price: {self._price}\n weight: {self._weight}")
+        print(f"Good name: {self.__name}\n price: {self.__price}\n weight: {self.__weight}")
 
 class Buy(Product):
-    def __init__(self, price, weight):
-        super().__init__(price, weight)
-        self._quantity = 10
+    def __init__(self, name, price, weight, quantity):
+        super().__init__(name, price, weight)
+        self.__quantity = quantity
 
     def calculate_total(self, ):
         temp1 = super().get_price()
         temp2 = super().get_weight()
-        total_price = temp1 * self._quantity
-        total_weight = temp2 * self._quantity
-        print(f"Goods bought: {self._quantity}\n Total price: {total_price}\n Total weight: {total_weight}")
+        total_price = temp1 * self.__quantity
+        total_weight = temp2 * self.__quantity
+        print(f"Goods bought: {self.__quantity}\n Total price: {total_price}\n Total weight: {total_weight}")
 
 class Check(Buy):
-    product = Product("Ice cream", 20, 500)
-    product.show_goods()
-    buy = Buy()
-    buy.calculate_total()
+    def __init__(self, name, price, weight, quantity):
+        super().__init__(name, price, weight, quantity)
 
-
-check = Check()
+check = Check("Ice cream", 20, 500, 3)
+check.show_goods()
+check.calculate_total()
