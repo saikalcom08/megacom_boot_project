a1a9007b02ecf53c6b23bb27df9c9a504fcbb488
saikalcom08 <saikalcom08@gmail.com>
2021-11-19 14:55:00 +0600

    updating urls.py and views.py

--- tasks/week 9/django_girls_app/urls.py
+++ tasks/week 9/django_girls_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name="all_posts"),
+    path('', views.post_list, name='post_list'),
     # path('detail/<int:pk>/', views.detail_post, name='detail'),
     # path('delete/<int:pk>/', views.delete_post, name='delete')
 ]
