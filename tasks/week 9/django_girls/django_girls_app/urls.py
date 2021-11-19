56e6abd62ca73b2d1dd34aa55405dd1b532024ae
saikalcom08 <saikalcom08@gmail.com>
2021-11-19 17:41:34 +0600

    Added views to create/edit blog post inside the site.

--- tasks/week 9/django_girls/django_girls_app/urls.py
+++ tasks/week 9/django_girls/django_girls_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    # path('delete/<int:pk>/', views.delete_post, name='delete')
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
