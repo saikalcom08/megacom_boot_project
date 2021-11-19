aff8068d9fed799121c08577ad6becf1b720ea60
saikalcom08 <saikalcom08@gmail.com>
2021-11-19 16:47:13 +0600

    Added view and template for detailed blog post as well as CSS for the site.

---
+++ tasks/week 9/django_girls/django_girls_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('delete/<int:pk>/', views.delete_post, name='delete')
+]
---
+++ tasks/week 9/django_girls/django_girls_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django_girls_app.models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
